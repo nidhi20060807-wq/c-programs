810d79e7d32ef5f1bab864f4fecf86de5737e7b2
nidhi20060807-wq <nidhi20060807@gmail.com>
2026-04-04 23:56:18 +0530

    Create file5.c

---
+++ file5.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main (){
+    FILE *fptr1;
+    FILE *fptr2;
+    char ch;
+
+    fptr1 = fopen("file1.txt","r");
+    if(fptr1 == NULL){
+        printf("File1 not opened\n");
+        return 1;
+    }
+
+    fptr2 = fopen("file2.txt","w");
+    if(fptr2 == NULL){
+        printf("File2 not opened\n");
+        return 1;
+    }
+
+    while((ch = fgetc(fptr1)) != EOF){
+        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
+            fputc(ch, fptr2);
+        }
+    }
+
+    printf("File copied successfully!\n");
+
+    fclose(fptr1);
+    fclose(fptr2);
+
+    return 0;
+}
